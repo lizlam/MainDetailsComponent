2fb7d9d94a23ba6df20643afa8bf461cda0bdf83
Liz <lizlam.217@gmail.com>
2020-08-24 11:02:16 -0700

    Add button to open from default to full.

--- src/App.js
+++ src/App.js
@@ -3,14 +3,24 @@ import MainDetails from "./MainDetails";
 import "./styles.css";
 
 export default function App() {
+  const mainContent = (
+    <div>
+      <br />
+      This is the main area.
+    </div>
+  );
+
+  const details = (
+    <div>
+      <br />
+      These are details.
+    </div>
+  );
   return (
     <div className="App">
       <h1>Main/Details Component</h1>
-      <h2>Testing Main Details Component</h2>
-      <MainDetails
-        main={<div>This is the main area.</div>}
-        details={<div>These are details.</div>}
-      />
+      <h2>A Simple Test</h2>
+      <MainDetails main={mainContent} details={details} />
     </div>
   );
 }
--- src/MainDetails.js
+++ src/MainDetails.js
@@ -8,6 +8,7 @@ const Container = styled.div`
 
 const MainTableArea = styled.div`
   width: 100%;
+  height: 400px;
   transition: width 1s;
   background-color: wheat;
 `;
@@ -17,9 +18,27 @@ const DetailsArea = styled.div`
   flex-grow: 1;
 `;
 
-const Divider = styled.button`
-  background-color: azure;
+const StyledSpan = styled.span`
+  background-color: cornsilk;
 `;
+
+const IconSpan = styled.span`
+  background-color: cornsilk;
+  border: blue 1px solid;
+  border-radius: 25px;
+  padding: 3px;
+  position: relative;
+  right: 14px;
+`;
+
+const CloseButton = styled.button`
+  float: right;
+`;
+
+const Right = `>>`;
+
+const Left = `<<`;
+
 const initialState = { details: "closed" };
 
 const reducer = (state, action) => {
@@ -30,13 +49,13 @@ const reducer = (state, action) => {
       return {
         details: "open",
         openType: "default",
-        style: { width: "25%", float: "left" }
+        style: { width: "25%", float: "left", borderRight: "1px solid blue" }
       };
     case "OPEN-FULL":
       return {
         details: "open",
         openType: "full",
-        style: { width: "0%", float: "left" }
+        style: { width: "0%", float: "left", borderRight: "1px solid blue" }
       };
     default:
       throw new Error();
@@ -59,19 +78,31 @@ export function MainDetails(props) {
   };
 
   const main = React.cloneElement(props.main, { onClick: openDefaultHandler });
+
   return (
     <Container>
       <MainTableArea style={state.style}>{main}</MainTableArea>
       {state.details === "open" && (
-        <Divider
-          onClick={
-            state.openType === "default" ? openFullHandler : openDefaultHandler
-          }
-        />
+        <>
+          <StyledSpan
+            onClick={
+              state.openType === "default"
+                ? openFullHandler
+                : openDefaultHandler
+            }
+          >
+            {state.openType === "default" ? (
+              <IconSpan>{Left}</IconSpan>
+            ) : (
+              <IconSpan>{Right}</IconSpan>
+            )}
+          </StyledSpan>
+          <div></div>
+        </>
       )}
       {state.details === "open" && (
         <DetailsArea>
-          <button onClick={closeHandler}>Close</button>
+          <CloseButton onClick={closeHandler}>Close</CloseButton>
           {props.details}
         </DetailsArea>
       )}
